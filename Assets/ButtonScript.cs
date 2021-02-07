e6ba61b91da4c6812373169d6ede6d6b96839f0d
jean.rouillier@gmail.com <Jannus499?.git>
2021-02-07 11:41:01 +0100

    init

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonScript : MonoBehaviour
+{
+    public Text resultText;
+    public Text historyText;
+
+    private List<String> stackTemplate = new List<String>{"BASIC", "BASIC", "BASIC", "ABILITY", "SPECIAL", "SPECIAL"};
+    private List<String> stack = new List<String>{"BASIC", "BASIC", "BASIC", "ABILITY", "SPECIAL", "SPECIAL"};
+   
+
+    public void draw(){
+        int count = stack.Count;
+        if(count == 0){
+            shuffle();
+        }
+        System.Random rand = new System.Random();
+        int index = rand.Next(count);
+        resultText.text = stack[index];
+        stack.RemoveAt(index);
+
+        historyText.text = "Restantes: "+stack.Count;
+    }
+
+    public void shuffle(){
+        stack = new List<string>(stackTemplate);
+        historyText.text = "Restantes: "+stack.Count;
+    }
+}
